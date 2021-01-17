6955471eccea4e9546b21def0d7d7e0dcd186d0e
spade-as-in-ace <73505788+spade-as-in-ace@users.noreply.github.com>
2021-01-17 17:37:30 -0500

    Add files via upload

---
+++ sleep-timer.ino
@@ -0,0 +1,31 @@
+// Automatic Device Switch with Sleep Timer
+// For: @Ade_Kolade
+#include <Chrono.h>
+
+#define THRESHOLD_VALUE 200
+#define SLEEP_TIME 5000
+#define SENSOR_PIN A0
+#define OUTPUT_PIN 9
+
+Chrono uptime;
+bool toggle;
+
+void setup() {
+    pinMode(OUTPUT_PIN, OUTPUT);
+    uptime.restart();
+    uptime.stop();
+}
+
+void loop() {
+    while(analogRead(SENSOR_PIN) > THRESHOLD_VALUE){
+        toggle = true;
+    }
+    if(toggle) {
+        digitalWrite(OUTPUT_PIN, HIGH);
+        uptime.restart();
+        while(!uptime.hasPassed(SLEEP_TIME));
+        uptime.stop();
+        digitalWrite(OUTPUT_PIN, LOW);
+        toggle = false;
+    }
+}
