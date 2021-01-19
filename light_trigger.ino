698605dc9f4938d34bb0177bd577fa8d60790c0a
spade-as-in-ace <73505788+spade-as-in-ace@users.noreply.github.com>
2021-01-19 01:43:47 -0500

    Update light_trigger.ino

--- light_trigger.ino
+++ light_trigger.ino
@@ -1,1 +1,21 @@
+// Automatic Trigger for Light Motion Sensor
+// For: @Ade_Kolade
 
+#define THRESHOLD_VALUE 200
+#define SENSOR_PIN A0
+#define OUTPUT_PIN 9
+
+void setup() {
+    pinMode(OUTPUT_PIN, OUTPUT);
+}
+
+void loop() {
+    while(analogRead(SENSOR_PIN) < THRESHOLD_VALUE) {
+        if(digitalRead(OUTPUT_PIN) == 0) {
+          digitalWrite(OUTPUT_PIN, 1);
+        }
+    }
+    if(digitalRead(OUTPUT_PIN) == 1) {
+      digitalWrite(OUTPUT_PIN, 0);
+    }
+}
