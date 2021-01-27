e4c3621216c51f5194f04f7fc9636f27ea45c305
spade-as-in-ace <73505788+spade-as-in-ace@users.noreply.github.com>
2021-01-27 01:06:49 -0500

    Create light-trigger-no-timer.ino

---
+++ light-trigger-no-timer.ino
@@ -0,0 +1,36 @@
+
+// Automatic Device Switch with Sleep Timer
+// For: @Ade_Kolade
+#include <Servo.h>
+
+#define THRESHOLD_VALUE 200
+#define SENSOR_PIN A0
+#define OUTPUT_PIN 9
+
+Servo servo;
+bool toggle;
+
+void setup() {
+    servo.attatch(OUTPUT_PIN);
+}
+
+void loop() {
+    while(analogRead(SENSOR_PIN) > THRESHOLD_VALUE){
+        toggle = true;
+    }
+    if(toggle) {
+        sweep();
+    }
+}
+
+void sweep() {
+    int pos = 0;
+    while(pos++ < 90) {
+        servo.write(pos);
+        delay(15);
+    }
+    while(pos-- > 0) {
+        servo.write(pos);
+        delay(15);
+    }
+}
